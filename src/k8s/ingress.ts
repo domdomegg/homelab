dd934b848740b334d09d08440b78c8a2547e57e5
Adam Jones <domdomegg+git@gmail.com>
2025-08-05 11:52:16 +0100

    fix(ingress): disable strict path validation
    Disable strict-validate-path-type to work around ingress-nginx bug
    affecting cert-manager ACME HTTP01 challenges.
    
    References:
    - https://cert-manager.io/docs/releases/release-notes/release-notes-1.18/#acme-http01-challenge-paths-now-use-pathtype-exact-in-ingress-routes
    - https://github.com/kubernetes/ingress-nginx/issues/11176

--- src/k8s/ingress.ts
+++ src/k8s/ingress.ts
@@ -17,6 +17,11 @@ export const ingress = new k8s.helm.v3.Release('ingress-nginx', {
     controller: {
       config: {
         'force-ssl-redirect': 'true',
+
+        // Disable strict path validation, to work around a bug in ingress-nginx
+        // https://cert-manager.io/docs/releases/release-notes/release-notes-1.18/#acme-http01-challenge-paths-now-use-pathtype-exact-in-ingress-routes
+        // https://github.com/kubernetes/ingress-nginx/issues/11176
+        'strict-validate-path-type': false,
       },
       ingressClassResource: {
         default: 'true',
