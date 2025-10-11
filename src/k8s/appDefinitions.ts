2a8f344e28a8cda8cfbaa9434f6ad09b017f5608
adam jones <domdomegg+git@gmail.com>
2025-10-11 05:14:21 +0100

    chore(deps): update ghcr.io/home-assistant/home-assistant:stable docker digest to 5ae78cf (#170)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- src/k8s/appDefinitions.ts
+++ src/k8s/appDefinitions.ts
@@ -22,7 +22,7 @@ export const apps: AppDefinition[] = [
     spec: {
       containers: [{
         name: 'ha',
-        image: 'ghcr.io/home-assistant/home-assistant:stable@sha256:9255033272ab8f7bede246109ea9e7302527faf3accbf2ba7ef619e2206107ad',
+        image: 'ghcr.io/home-assistant/home-assistant:stable@sha256:5ae78cf2e6d8b53439cac50ed184ff1336cfbbb878fce16f75589e048fcdcdd9',
         // Necessary to access the Bluetooth
         securityContext: {
           privileged: true,
